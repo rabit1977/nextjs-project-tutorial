17735af7e65e019cf0436b13da7e66a1e4dae322
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-09-29 23:37:03 +0200

    comming to the end by this week....

---
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { IconType } from 'react-icons';
+
+interface CategoryItemProps {
+  label: string;
+  value?: string;
+  icon?: IconType;
+}
+
+export const CategoryItem = ({
+  label,
+  value,
+  icon: Icon,
+}: CategoryItemProps) => {
+  return (
+    <button
+      className={cn(
+        'py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition'
+        // TODO: change style if active
+      )}
+      type='button'
+    >
+      {Icon && <Icon size={20} />}
+      <div className='truncate'>{label}</div>
+    </button>
+  );
+};
