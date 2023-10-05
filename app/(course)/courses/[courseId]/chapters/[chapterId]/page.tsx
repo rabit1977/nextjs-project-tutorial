98cc1c72df0e5c33d46fbc43064effaeb1dc3ccf
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-10-06 00:36:31 +0200

    okay, almost done

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,25 +1,27 @@
-import { getChapter } from '@/actions/get-chapter';
-import { Banner } from '@/components/banner';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import { VideoPlayer } from './_components/video-player';
-import { CourseEnrollButton } from './_components/course-enroll-button';
+import { File } from 'lucide-react';
+
+import { getChapter } from '@/actions/get-chapter';
+import { Banner } from '@/components/banner';
 import { Separator } from '@/components/ui/separator';
 import { Preview } from '@/components/preview';
+
+import { VideoPlayer } from './_components/video-player';
+import { CourseEnrollButton } from './_components/course-enroll-button';
+import { CourseProgressButton } from './_components/course-progress-button';
 
 const ChapterIdPage = async ({
   params,
 }: {
-  params: {
-    courseId: string;
-    chapterId: string;
-  };
+  params: { courseId: string; chapterId: string };
 }) => {
   const { userId } = auth();
 
   if (!userId) {
     return redirect('/');
   }
+
   const {
     chapter,
     course,
@@ -39,14 +41,13 @@ const ChapterIdPage = async ({
   }
 
   const isLocked = !chapter.isFree && !purchase;
-  const completedOnEnd = !!purchase && !userProgress?.isCompleted;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
 
   return (
     <div>
       {userProgress?.isCompleted && (
         <Banner variant='success' label='You already completed this chapter.' />
       )}
-
       {isLocked && (
         <Banner
           variant='warning'
@@ -62,14 +63,19 @@ const ChapterIdPage = async ({
             nextChapterId={nextChapter?.id!}
             playbackId={muxData?.playbackId!}
             isLocked={isLocked}
-            completeOnEnd={completedOnEnd}
+            completeOnEnd={completeOnEnd}
           />
         </div>
         <div>
-          <div className='p-4 flex flex-col md:flex-row items-center'>
-            <h2 className='text2xl font-semibold mb-2'>{chapter.title}</h2>
+          <div className='p-4 flex flex-col md:flex-row items-center justify-between'>
+            <h2 className='text-2xl font-semibold mb-2'>{chapter.title}</h2>
             {purchase ? (
-              <div>{/* TODO: Add CourseProgress Button */}</div>
+              <CourseProgressButton
+                chapterId={params.chapterId}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                isCompleted={!!userProgress?.isCompleted}
+              />
             ) : (
               <CourseEnrollButton
                 courseId={params.courseId}
@@ -85,11 +91,17 @@ const ChapterIdPage = async ({
             <>
               <Separator />
               <div className='p-4'>
-                {attachments.map((attachment) => {
-                  <a href={attachment.url} target='_blank' key={attachment.id}>
-                    <p>{attachment.id}</p>
-                  </a>;
-                })}
+                {attachments.map((attachment) => (
+                  <a
+                    href={attachment.url}
+                    target='_blank'
+                    key={attachment.id}
+                    className='flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline'
+                  >
+                    <File />
+                    <p className='line-clamp-1'>{attachment.name}</p>
+                  </a>
+                ))}
               </div>
             </>
           )}
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -1,4 +1,4 @@
-'use client';
+ 'use client';
 
 import { FileUpload } from '@/components/file-upload';
 import { Button } from '@/components/ui/button';
